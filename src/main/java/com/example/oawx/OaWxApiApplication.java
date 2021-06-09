52b42f470379d46d9e9317b27f0fdbbb212948f7
lwy <2020783236@qq.com>
2021-06-09 17:46:09 +0800

    防止xss攻击

--- src/main/java/com/example/oawx/OaWxApiApplication.java
+++ src/main/java/com/example/oawx/OaWxApiApplication.java
@@ -2,8 +2,10 @@ package com.example.oawx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class OaWxApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/oawx/controller/form/TestSayHelloForm.java
+++ src/main/java/com/example/oawx/controller/form/TestSayHelloForm.java
@@ -10,8 +10,8 @@ import javax.validation.constraints.Pattern;
 @Data
 @ApiModel
 public class TestSayHelloForm {
-    @NotBlank
-    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "不符合正则表达式")
+//    @NotBlank
+//    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "不符合正则表达式")
     @ApiModelProperty("姓名")
     private String name;
 }
